0705d03f0161545d2780845cd1757d0c8281f8d6
senpai4 <44644110+senpai4@users.noreply.github.com>
2018-10-31 23:08:05 +0800

    56

--- main.lua
+++ main.lua
@@ -3,5 +3,5 @@
 -- main.lua
 --
 -----------------------------------------------------------------------------------------
-
--- Your code here
+local msgText = display.newText( "Hello,world", display.contentHeight * 0.5,display.contentWidth*0.5, native.systemFont, 32 )
+msgText:setFillColor( 0.2, 0.6, 0.8 )
